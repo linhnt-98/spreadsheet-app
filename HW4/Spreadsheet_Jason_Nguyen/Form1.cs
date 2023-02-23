d154af974c03ffae18570736f90a4f379ec2e85b
lnguyen6 <lnguyen6@eecs.wsu.edu>
2023-02-23 03:09:52 -0800

    Add Columns

---
+++ HW4/Spreadsheet_Jason_Nguyen/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spreadsheet_Jason_Nguyen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDataGrid();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void InitializeDataGrid()
+        {
+
+            dataGridView1.Columns.Clear();
+            dataGridView1.Rows.Clear();
+
+            for (char c = 'A'; c <= 'Z'; c++)
+            {
+                dataGridView1.Columns.Add("col" + Char.ToString(c), Char.ToString(c)); 
+            }
+
+            
+            for (int i = 1; i <= 50; i++)
+            {
+                dataGridView1.Rows.Add(1) ;
+                dataGridView1.Rows[i - 1].HeaderCell.Value = i.ToString();
+            }
+
+        }
+    }
+}
