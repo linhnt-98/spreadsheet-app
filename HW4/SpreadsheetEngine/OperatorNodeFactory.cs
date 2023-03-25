130da67ee0bd92c628e581f7912a5d88b4cb0b88
lnguyen6 <lnguyen6@eecs.wsu.edu>
2023-03-24 23:16:36 -0700

    Update Test

--- HW4/SpreadsheetEngine/OperatorNodeFactory.cs
+++ HW4/SpreadsheetEngine/OperatorNodeFactory.cs
@@ -1,12 +1,142 @@
-﻿using System;
+﻿using HW5;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SpreadsheetEngine
 {
-    internal class Class1
+    /// <summary>
+    /// OperatorNodeFactory class.
+    /// </summary>
+    internal class OperatorNodeFactory
     {
+        /// <summary>
+        /// A dictionary to key char values to their types.
+        /// </summary>
+        private readonly Dictionary<char, Type> operators = new Dictionary<char, Type>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OperatorNodeFactory"/> class.
+        /// </summary>
+        public OperatorNodeFactory()
+        {
+            this.LookForOperator((op, type) => this.operators.Add(op, type));
+        }
+
+        /// <summary>
+        /// To pass the OnOperator Method as an argument.
+        /// </summary>
+        /// <param name="op">Operator.</param>
+        /// <param name="type">Type.</param>
+        private delegate void OnOperator(char op, Type type);
+
+        /// <summary>
+        /// To check if operator is a key in the operator dictionary.
+        /// </summary>
+        /// <param name="op">Operator.</param>
+        /// <returns>Boolean.</returns>
+        public bool IsAOperator(char op)
+        {
+            return this.operators.ContainsKey(op);
+        }
+
+        /// <summary>
+        /// Returns the precedence of the given operator.
+        /// </summary>
+        /// <param name="op">operator.</param>
+        /// <returns>ushort.</returns>
+        public ushort Precedence(char op)
+        {
+            if (this.IsAOperator(op))
+            {
+                Type type = this.operators[op];
+                PropertyInfo propertyInfo = type.GetProperty("Precedence");
+                if (propertyInfo != null)
+                {
+                    object value = propertyInfo.GetValue(type);
+                    if (value.GetType().Name == "UInt16")
+                    {
+                        return (ushort)value;
+                    }
+                }
+            }
+
+            // else return nothing
+            return 0;
+        }
+
+        /// <summary>
+        /// Returns the Associativity of the given operator.
+        /// </summary>
+        /// <param name="op">Operator.</param>
+        /// <returns>int.</returns>
+        public OperatorNode.AssocEnum Associativity(char op)
+        {
+            if (this.IsAOperator(op))
+            {
+                Type type = this.operators[op];
+                PropertyInfo propertyInfo = type.GetProperty("Associativity");
+                if (propertyInfo != null)
+                {
+                    object value = propertyInfo.GetValue(type);
+                    if (value.GetType().Name == "Associative")
+                    {
+                        return (OperatorNode.AssocEnum)value;
+                    }
+                }
+            }
+
+            // default return left if nothing is found.
+            return OperatorNode.AssocEnum.Left;
+        }
+
+        /// <summary>
+        /// Creates an operator Node.
+        /// </summary>
+        /// <param name="op">operator.</param>
+        /// <returns>Operator node.</returns>
+        public OperatorNode CreateOperatorNode(char op)
+        {
+            if (this.IsAOperator(op))
+            {
+                object operatorNodeObject = Activator.CreateInstance(this.operators[op]);
+                if (operatorNodeObject is OperatorNode node)
+                {
+                    return node;
+                }
+            }
+
+            // else, throw exception.
+            throw new Exception("No Operator");
+        }
+
+        /// <summary>
+        /// Check if operator is available.
+        /// </summary>
+        /// <param name="onOperator">a delegate with operator op type.</param>
+        private void LookForOperator(OnOperator onOperator)
+        {
+            Type operatorNodeType = typeof(OperatorNode);
+            foreach (var assemply in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                IEnumerable<Type> operatorTypes = assemply.GetTypes().Where(type => type.IsSubclassOf(operatorNodeType));
+
+                foreach (var type in operatorTypes)
+                {
+                    PropertyInfo operatorField = type.GetProperty("Operator");
+                    if (operatorField != null)
+                    {
+                        object value = operatorField.GetValue(type);
+                        if (value is char operatorSymbol)
+                        {
+                            onOperator(operatorSymbol, type);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- HW4/SpreadsheetTest/Test.cs
+++ HW4/SpreadsheetTest/Test.cs
@@ -1,3 +1,4 @@
+using HW5;
 using SpreadsheetEngine;
 
 namespace SpreadsheetTest
@@ -9,7 +10,6 @@ namespace SpreadsheetTest
     public class Tests
     {
         // Instanciates a Spreadsheet.
-        // Im kiddo now :D anw xong roai
         private readonly Spreadsheet spreadSheetTest = new Spreadsheet(4, 4);
 
         /// <summary>
@@ -18,8 +18,91 @@ namespace SpreadsheetTest
         [Test]
         public void TestSpreadSheet()
         {
-            Assert.That(spreadSheetTest.RowCount, Is.EqualTo(4));
-            Assert.That(spreadSheetTest.ColumnCount, Is.EqualTo(4));
+            Assert.That(this.spreadSheetTest.RowCount, Is.EqualTo(4));
+            Assert.That(this.spreadSheetTest.ColumnCount, Is.EqualTo(4));
+        }
+
+        /// <summary>
+        /// Testing normal expression cases.
+        /// </summary>
+        [Test]
+        public void ExpressionTreeNormalCaseTests()
+        {
+            ExpressionTree testAddition = new ExpressionTree("5+5");
+            Assert.That(testAddition.Evaluate(), Is.EqualTo(10.0));
+
+            ExpressionTree testSubtraction = new ExpressionTree("5-1");
+            Assert.That(testSubtraction.Evaluate(), Is.EqualTo(4.0));
+
+            ExpressionTree testMultiplication = new ExpressionTree("5*5");
+            Assert.That(testMultiplication.Evaluate(), Is.EqualTo(25.0));
+
+            ExpressionTree testDivision = new ExpressionTree("3/6");
+            Assert.That(testDivision.Evaluate(), Is.EqualTo(0.5));
+
+            ExpressionTree testAdditionSeries = new ExpressionTree("1+2+3+4");
+            Assert.That(testAdditionSeries.Evaluate(), Is.EqualTo(10.0));
+
+            ExpressionTree testMultiplicationSeries = new ExpressionTree("1*2*3*4");
+            Assert.That(testMultiplicationSeries.Evaluate(), Is.EqualTo(24.0));
+
+            ExpressionTree testDivisionSeries = new ExpressionTree("100/5/2/5");
+            Assert.That(testDivisionSeries.Evaluate(), Is.EqualTo(2.0));
+        }
+
+        /// <summary>
+        /// Testing boundary expression cases.
+        /// </summary>
+        [Test]
+        public void ExpressionTreeBoundaryCaseTests()
+        {
+            ExpressionTree testDivisionBoundary = new ExpressionTree("0/65");
+            Assert.That(testDivisionBoundary.Evaluate(), Is.EqualTo(0));
+
+            ExpressionTree testMultiplicationBoundary = new ExpressionTree("0*55");
+            Assert.That(testMultiplicationBoundary.Evaluate(), Is.EqualTo(0));
+        }
+
+        /// <summary>
+        /// Testing edge expression cases.
+        /// </summary>
+        [Test]
+        public void ExpressionTreeEdgeCaseTests()
+        {
+            ExpressionTree testInvalidExpression = new ExpressionTree("5/0");
+            Assert.That(testInvalidExpression.Evaluate(), Is.EqualTo(double.PositiveInfinity));
+
+            ExpressionTree testInvalidInput = new ExpressionTree("string");
+            Assert.That(testInvalidInput.Evaluate(), Is.EqualTo(double.NaN));
+        }
+
+        /// <summary>
+        /// Testing parentheses in expression.
+        /// </summary>
+        [Test]
+        public void ParenthesesNormalCaseTests()
+        {
+            ExpressionTree additionParentTest = new ExpressionTree("7+(7+7)");
+            Assert.That(additionParentTest.Evaluate(), Is.EqualTo(21));
+
+            ExpressionTree multiplicationParentTest = new ExpressionTree("(6+6)*6");
+            Assert.That(multiplicationParentTest.Evaluate(), Is.EqualTo(72));
+
+            ExpressionTree divisionParentTest = new ExpressionTree("(6+6)/6");
+            Assert.That(divisionParentTest.Evaluate(), Is.EqualTo(2));
+        }
+
+        /// <summary>
+        /// Testing order of operations.
+        /// </summary>
+        [Test]
+        public void ExpressionOrderTests()
+        {
+            ExpressionTree multiplicationOrderTest = new ExpressionTree("8+8*3+7");
+            Assert.That(multiplicationOrderTest.Evaluate(), Is.EqualTo(39));
+
+            ExpressionTree divisionOrderTest = new ExpressionTree("8+9/3+7");
+            Assert.That(multiplicationOrderTest.Evaluate(), Is.EqualTo(18));
         }
     }
 }
